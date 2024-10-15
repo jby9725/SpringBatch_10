2634cc3ca6bb8ea3fb73a4380d55950582632cab
jby9725 <jby9725@gmail.com>
2024-10-15 15:55:41 +0900

    DB.sql 추가

---
+++ DB.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS `SpringBatch_10`;
+CREATE DATABASE `SpringBatch_10`;
+USE `SpringBatch_10`;
+
+DROP DATABASE IF EXISTS `SpringBatch_10_backup`;
+CREATE DATABASE `SpringBatch_10_backup`;
+USE `SpringBatch_10_backu`BATCH_JOB_EXECUTION`p`;
+
+# 배치 기본 테이블 + (사이즈 더 커지면) 커스텀 테이블)
+SHOW TABLES;
